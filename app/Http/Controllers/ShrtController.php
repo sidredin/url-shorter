11dfc12d5d9e7468cda9e1463c3969969c596553
sadr3479 <sadr3479@gmail.com>
2022-07-08 11:28:59 +0300

    stats table and Stat model added

--- app/Http/Controllers/ShrtController.php
+++ app/Http/Controllers/ShrtController.php
@@ -1,9 +1,12 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
 use App\Models\Link;
+use App\Models\Stat;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Redirector;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
@@ -15,10 +18,16 @@ class ShrtController extends Controller
      * @param int $id
      * @return Redirector|RedirectResponse
      */
-    public function show($id): Redirector|RedirectResponse
+    public function show(Request $request, $id): Redirector|RedirectResponse
     {
         $link = Link::find($id);
         if ($link === null) throw new HttpException(404, 'Ссылка не найдена');
+
+        $stat = Stat::create([
+            'ip' => $request->ip(),
+            'user_agent' => $request->header('user-agent'),
+            'link_id' => $id,
+        ]);
 
 
         return redirect($link->long_url);
